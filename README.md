5ee30f12c497d07fc451c9ab08558e81c2b45b9d
Martyn Davis <mcd@marengo.cc>
2024-08-14 14:20:52 +0100

    Added catch2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## A Quick CMake Starter Framework
+
+`brew install catch2`
+
 For use in VSCode. To select debug or release run "CMake: Select Configure Preset"
 
 If tests don't show up in the test explorer, ensure that `"cmake.ctest.testExplorerIntegrationEnabled": true` is set
--- tests.cpp
+++ tests.cpp
@@ -1,10 +1,21 @@
+#define CATCH_CONFIG_MAIN
 #include "hellolib.h"
 
-int main()
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("Do stuff")
 {
-    if (sumUp(10, 5) == 15) {
-        return 0;
-    } else {
-        return 1;
-    }
+    REQUIRE(sumUp(1,1) == 2);
+}
+TEST_CASE("Do more stuff")
+{
+    REQUIRE(sumUp(50, 4) == 54);
+}
+TEST_CASE("Do even more stuff")
+{
+    REQUIRE(sumUp(6, 63) == 69);
+}
+TEST_CASE("Do final stuff")
+{
+    REQUIRE(sumUp(80'000, 1'544) == 8'1544);
 }
